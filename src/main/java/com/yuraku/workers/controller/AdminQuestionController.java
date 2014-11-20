bda6d2588683a8fc899bdc1c5d5a41e81104ca29
guriyan911 <yanotatsuya@192.168.0.5>
2014-11-20 21:57:28 +0900

    複数データソースでの接続確認（但し汚いので修正要）

--- src/main/java/com/yuraku/workers/controller/AdminQuestionController.java
+++ src/main/java/com/yuraku/workers/controller/AdminQuestionController.java
@@ -1,8 +1,16 @@
 package com.yuraku.workers.controller;
 
+import javax.sql.DataSource;
 import javax.validation.Valid;
 
+import lombok.val;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +21,17 @@ import com.yuraku.workers.domain.TtQuizeesRepository;
 
 @Controller
 public class AdminQuestionController extends BaseController {
-
+	
+	@Autowired 
+    @Qualifier("jdbcGenbu") 
+    protected JdbcTemplate jdbc;
+	
     @Autowired
     TtQuizeesRepository ttQuizeesRepository;
     
     @RequestMapping(value="/admin_question_input", method=RequestMethod.GET)
     public String input(TtQuizees ttQuizees){
+    	val test= jdbc.queryForMap("SELECT * FROM m_my_rule");
         return "admin_question_input";
     }
 
@@ -27,7 +40,12 @@ public class AdminQuestionController extends BaseController {
     	if (bindingResult.hasErrors()) {
             return "admin_question_input";
         }
-        return "redirect:/admin_question_result";
+    	
+    	ttQuizeesRepository.save(ttQuizees);
+    	
+    	// もし一覧画面から登録画面、そして登録後に一覧画面に戻るとかならredirectを使う
+        //return "redirect:/admin_question_result";
+        return "admin_question_result";
     }
 }
 
